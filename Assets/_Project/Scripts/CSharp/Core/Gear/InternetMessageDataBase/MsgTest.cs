6596fb244a96a424e7645db695fd9bae80509aba
unknown <jiazhengbo@devel.sixcube.cn>
2026-09-04 18:02:50 +0800

    优化InternetMessage ID注入、解读逻辑

---
+++ Assets/_Project/Scripts/CSharp/Core/Gear/InternetMessageDataBase/MsgTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MsgTest : InternetMessageBase
+{
+    public static uint id
+    {
+        get
+        {
+            return MESSAGEID;
+        }
+    }
+}
